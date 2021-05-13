328f5299616b4ed0472d63e51cd4ef47114c52b2
Daniel Jorgens <djoerch@gmail.com>
2021-05-13 04:39:16 +0200

    ENH: add resume training option.

--- scripts/segsrgan_run_training.sh
+++ scripts/segsrgan_run_training.sh
@@ -9,9 +9,12 @@ WEIGHTS_FOLDER="weights_test"
 LOG_FOLDER="logs"
 
 
+CONTINUE_FROM_EPOCH=10
+
+
 cmd="python SegSRGAN/SegSRGAN_training.py"
 args=()
-args+=(-e 1)  # num epochs
+args+=(-e 100)  # num epochs
 args+=(--new_low_res "0.35 2.8 0.35")  # minimum resolution
 args+=(--new_low_res "0.55 3.5 0.55")  # maximum resolution
 args+=(--csv "${PATH_TO_DATA_ROOT}/splits/training.csv")
@@ -20,5 +23,9 @@ args+=(--dice_file "${PATH_TO_DATA_ROOT}/${LOG_FOLDER}/dice.csv")
 args+=(--mse_file "${PATH_TO_DATA_ROOT}/${LOG_FOLDER}/mse.csv")
 args+=(--folder_training_data "${TMP_FOLDER}")
 args+=(--number_of_disciminator_iteration 1)
+
+# comment out next 2 lines to deactivate continued training
+args+=(--init_epoch $((CONTINUE_FROM_EPOCH+1)))  # last_epoch + 1
+args+=(--weights "${PATH_TO_DATA_ROOT}/${WEIGHTS_FOLDER}/SegSRGAN_epoch_${CONTINUE_FROM_EPOCH}")
 
 ${cmd} ${args[@]}

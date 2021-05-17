3f81b0a336f3bb623725dccf96470341a74cea32
Daniel Jorgens <djoerch@gmail.com>
2021-05-17 15:36:18 +0200

    ENH: introduce experiment folder level in output folder structure.

--- scripts/segsrgan_run_test.sh
+++ scripts/segsrgan_run_test.sh
@@ -3,9 +3,9 @@
 
 PATH_TO_DATA_ROOT="/home/daniel/scratch"
 
-OUTPUT_IMG_FOLDER="output"
+OUTPUT_IMG_FOLDER="output_LC"
 WEIGHTS_FOLDER="weights_test"
-WEIGHTS_NAME="SegSRGAN_epoch_1"
+WEIGHTS_NAME="SegSRGAN_epoch_100"
 
 PATCH_SIZE=64
 STEP_SIZE=32
@@ -13,10 +13,10 @@ STEP_SIZE=32
 
 cmd="python SegSRGAN/job_model.py"
 args=()
-args+=(--path "${PATH_TO_DATA_ROOT}/splits/test.csv")
+args+=(--path "${PATH_TO_DATA_ROOT}/splits/test_LC.csv")
 args+=(--patch ${PATCH_SIZE})
 args+=(--step ${STEP_SIZE})
-args+=(--result_folder_name "${PATH_TO_DATA_ROOT}/output/${WEIGHTS_NAME}")
+args+=(--result_folder_name "${PATH_TO_DATA_ROOT}/${OUTPUT_IMG_FOLDER}/${WEIGHTS_NAME}")
 args+=(--weights_path "${PATH_TO_DATA_ROOT}/${WEIGHTS_FOLDER}/${WEIGHTS_NAME}")
 
 ${cmd} ${args[@]}
--- scripts/segsrgan_run_training.sh
+++ scripts/segsrgan_run_training.sh
@@ -3,6 +3,8 @@
 
 PATH_TO_DATA_ROOT="/home/daniel/scratch"
 
+EXP_FOLDER="discriminator_2"
+
 TMP_FOLDER="/mnt/data/Episurfsr/tmp"
 
 WEIGHTS_FOLDER="weights_test"
@@ -18,14 +20,14 @@ args+=(-e 100)  # num epochs
 args+=(--new_low_res "0.35 2.8 0.35")  # minimum resolution
 args+=(--new_low_res "0.55 3.5 0.55")  # maximum resolution
 args+=(--csv "${PATH_TO_DATA_ROOT}/splits/training.csv")
-args+=(--snapshot_folder "${PATH_TO_DATA_ROOT}/${WEIGHTS_FOLDER}")
-args+=(--dice_file "${PATH_TO_DATA_ROOT}/${LOG_FOLDER}/dice.csv")
-args+=(--mse_file "${PATH_TO_DATA_ROOT}/${LOG_FOLDER}/mse.csv")
+args+=(--snapshot_folder "${PATH_TO_DATA_ROOT}/${EXP_FOLDER}/${WEIGHTS_FOLDER}")
+args+=(--dice_file "${PATH_TO_DATA_ROOT}/${EXP_FOLDER}/${LOG_FOLDER}/dice.csv")
+args+=(--mse_file "${PATH_TO_DATA_ROOT}/${EXP_FOLDER}/${LOG_FOLDER}/mse.csv")
 args+=(--folder_training_data "${TMP_FOLDER}")
-args+=(--number_of_disciminator_iteration 1)
+args+=(--number_of_disciminator_iteration 2)
 
 # comment out next 2 lines to deactivate continued training
-args+=(--init_epoch $((CONTINUE_FROM_EPOCH+1)))  # last_epoch + 1
-args+=(--weights "${PATH_TO_DATA_ROOT}/${WEIGHTS_FOLDER}/SegSRGAN_epoch_${CONTINUE_FROM_EPOCH}")
+#args+=(--init_epoch $((CONTINUE_FROM_EPOCH+1)))  # last_epoch + 1
+#args+=(--weights "${PATH_TO_DATA_ROOT}/${EXP_FOLDER}/${WEIGHTS_FOLDER}/SegSRGAN_epoch_${CONTINUE_FROM_EPOCH}")
 
 ${cmd} ${args[@]}
